3cc96f66366d7116d66e152946aaf56881844182
Steven Siu <wing199901@gmail.com>
2019-06-20 18:17:13 +0800

    Update make_order.php

--- make_order.php
+++ make_order.php
@@ -27,9 +27,18 @@
             border-bottom: 1px solid #ddd;
         }
 
+        tr:first-child th:first-child{
+            border-top-left-radius: 10px;
+        }
+
+        tr:first-child th:last-child {
+            border-top-right-radius: 10px;
+        }
+
         tr:last-child td:first-child{
             border-bottom-left-radius: 10px;
         }
+
         tr:last-child td:last-child {
             border-bottom-right-radius: 10px;
         }
