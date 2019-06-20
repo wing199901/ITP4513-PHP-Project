b1f33017e1e6ed3183ef855ab0fa4ac82b16a39e
Steven Siu <wing199901@gmail.com>
2019-06-20 13:01:39 +0800

    Update make_order.php

--- make_order.php
+++ make_order.php
@@ -34,11 +34,15 @@
     require_once( 'conn.php' );
     $sql = "SELECT * FROM Part WHERE stockStatus = 1 AND stockQuantity > 0 ";
     $rs = mysqli_query( $conn, $sql );
+
     echo '<form method="POST" action="$_SERVER[PHP_SELF]">';
     echo '<table border="1"><tr>
             <th>Part Number</th><th>Email</th><th>Part Name</th><th>Quantity</th><th>Price</th><th>Status</th>';
     while ( $rc = mysqli_fetch_assoc( $rs ) )
         printf( '<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td</tr>', $rc[ 'partNumber' ], $rc[ 'email' ], $rc[ 'partName' ], $rc[ 'stockQuantity' ], $rc[ 'stockPrice' ], $rc[ 'stockStatus' ] );
+    echo '</table>';
+    mysqli_free_result( $rs );
+    mysqli_close( $conn );
     ?>
 
 </body>
