4c2564ed491e66820d6e77bfbb33904615ba6f77
Steven Siu <wing199901@gmail.com>
2019-06-20 20:35:34 +0800

    20:35

--- make_order.php
+++ make_order.php
@@ -4,9 +4,44 @@
 <head>
     <meta charset="UTF-8">
     <title>Make Order</title>
-    <link rel="icon" href="favicon.ico" type="image/x-icon"/>
-    <link href="css/font.css" rel="stylesheet" type="text/css"/>
-    <link href="css/index.css" rel="stylesheet" type="text/css"/>
+    <link rel="icon" href="favicon.ico" type="image/x-icon" />
+    <link href="css/font.css" rel="stylesheet" type="text/css" />
+    <link href="css/index.css" rel="stylesheet" type="text/css" />
+    <link href="css/table.css" rel="stylesheet" type="text/css" />
+
+    <style>
+        input[type=text] {
+            padding: 12px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+
+        input[type=submit] {
+            font-size: 25px;
+            border: 10px;
+            border-radius: 5px;
+            padding: 10px;
+            width: 200px;
+        }
+
+        input[type=reset] {
+            font-size: 25px;
+            border: 10px;
+            border-radius: 5px;
+            padding: 10px;
+            width: 200px;
+        }
+
+        div {
+            text-align: center;
+            width: auto;
+            height: auto;
+        }
+
+        div .submit {
+            float: inherit;
+        }
+    </style>
 </head>
 
 <body class="font">
@@ -25,24 +60,24 @@
         </li>
         <li><a href="login.html" onclick="return confirm('Are you sure you want to sign out?')">Log Out</a>
         </li>
-        </div>
-        </li>
     </ul>
     <br>
 
     <?php
-    require_once( 'conn.php' );
+    require_once('conn.php');
     $sql = "SELECT * FROM Part WHERE stockStatus = 1 AND stockQuantity > 0 ";
-    $rs = mysqli_query( $conn, $sql );
+    $rs = mysqli_query($conn, $sql);
 
     echo '<form method="POST" action="$_SERVER[PHP_SELF]">';
-    echo '<table border="1"><tr>
-            <th>Part Number</th><th>Email</th><th>Part Name</th><th>Quantity</th><th>Price</th><th>Status</th>';
-    while ( $rc = mysqli_fetch_assoc( $rs ) )
-        printf( '<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td</tr>', $rc[ 'partNumber' ], $rc[ 'email' ], $rc[ 'partName' ], $rc[ 'stockQuantity' ], $rc[ 'stockPrice' ], $rc[ 'stockStatus' ] );
-    echo '</table>';
-    mysqli_free_result( $rs );
-    mysqli_close( $conn );
+    echo '<div><table align="center"><tr>
+            <th>Part Number</th><th>Email</th><th>Part Name</th><th>Quantity</th><th>Price</th><th>Status</th><th>Purchase</th>';
+    while ($rc = mysqli_fetch_assoc($rs))
+        printf('<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td><input type="text" name="purchase" id="purchase"></td></tr>', $rc['partNumber'], $rc['email'], $rc['partName'], $rc['stockQuantity'], $rc['stockPrice'], $rc['stockStatus']);
+    echo '</table><br>
+        <div class="submit">Delivery Address:<input type="text" name="Address">&emsp;
+        <input type="submit">&emsp;<input type="reset"></div></div></form>';
+    mysqli_free_result($rs);
+    mysqli_close($conn);
     ?>
 
 </body>
