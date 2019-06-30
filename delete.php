514cf664e0fce590e9253d6ad859de668865f52b
Steven Siu <wing199901@gmail.com>
2019-06-30 21:30:39 +0800

    Update delete.php

--- delete.php
+++ delete.php
@@ -3,6 +3,8 @@
 require('conn.php');
 
 if (isset($_GET['orderID'])) {
+    $sql = "DELETE FROM OrderPart WHERE orderID =  " . $_GET['orderID'];
+    mysqli_query($conn, $sql);
     $sql = "DELETE FROM Orders WHERE orderID =  " . $_GET['orderID'];
     mysqli_query($conn, $sql);
     mysqli_close($conn);
