dd08e917ff0b99af4b223a75e445ea45a1ebf41b
Steven Siu <wing199901@gmail.com>
2019-06-30 19:53:59 +0800

    Update delivery.php

--- delivery.php
+++ delivery.php
@@ -6,7 +6,13 @@ if (isset($_GET['orderID'])) {
     $sql = "UPDATE Orders SET `status` = 2 WHERE orderID =  " . $_GET['orderID'];
     $rs = mysqli_query($conn, $sql);
     mysqli_free_result($rs);
+    $sql = "SELECT * FROM OrderPart WHERE orderID =  " . $_GET['orderID'];
+    $rs = mysqli_query($conn, $sql);
+
+    while($rc=mysqli_fetch_assoc($rs)){
+        $sql="UPDATE Part SET stockQuantity = StockQuantity - $rc[quantity] WHERE partNumber = $rc[partNumber]";
+        mysqli_query($conn, $sql);
+    }
     mysqli_close($conn);
     echo 'Delivered successfully.';
 }
-?>
